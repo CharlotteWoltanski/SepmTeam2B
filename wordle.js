e80c82ad463c067ef44f1e81926952b4bab4ed5d
Charlotte <62271262+CharlotteWoltanski@users.noreply.github.com>
2022-05-01 21:49:15 +1000

    Keyboard update again

--- wordle.js
+++ wordle.js
@@ -7,6 +7,11 @@ var column = 0;
 
 var gameOver = false;
 
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+
 
 var wordList= ["pears","jello","bagel","river","hyped"];
 var guessList = ["pears","jello","bagel","river","hyped","cargo", "river","mango"];
@@ -32,4 +37,47 @@ function initialize(){
 
         }
     }
+
+let keyboard = [
+    ["Q","W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H","J","K","L", " "],
+    ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"]
+]
+for(let i = 0; i < keyboard.length; i++){
+    let currentRow = keyboard[i];
+    let keyboardRow = document.createElement("div");
+    keyboardRow.classList.add("keyboard-row");
+
+    for(let j=0; j < currentRow.length; j++){
+        let keyTile = document.createElement("div");
+        let key = currentRow[j];
+        keyTile.innerText = key;
+        if (key == "Enter"){
+            keyTile.id = "Enter";
+        }
+        else if (key == "⌫" ){
+            keyTile.id = "Backspace";
+        }
+        else if("A" <= key && key <= "Z"){
+            keyTile.id = "Key" + key;
+        }
+        keyTile.addEventListener("click",processKey);
+
+        if (key == "Enter"){
+            keyTile.classList.add("enter-key-tile");
+        } else {
+            keyTile.classList.add("key-tile");
+        }
+        keyboardRow.appendChild(keyTile);
+    }
+        document.body.appendChild(keyboardRow)
+    }
+    document.addEventListener("keyup", (e) => {
+        processInput(e);
+    })
+}
+
+function processKey(){
+    e = {"code" : this.id};
+    processInput(e);
 }
