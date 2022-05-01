ba38421a5bc700741f3affe59859b4bb9f2269ae
Charlotte <62271262+CharlotteWoltanski@users.noreply.github.com>
2022-05-01 21:16:03 +1000

    Keyboard skeleton added

--- wordle.js
+++ wordle.js
@@ -1,5 +1,5 @@
 
-var heigh =6;
+var height =6;
 var width = 5;
 
 var row = 0;
@@ -7,3 +7,29 @@ var column = 0;
 
 var gameOver = false;
 
+
+var wordList= ["pears","jello","bagel","river","hyped"];
+var guessList = ["pears","jello","bagel","river","hyped","cargo", "river","mango"];
+
+
+guessList = guessList.concat(wordList);
+
+var word =wordList[Math.floor(Math.random()*wordList.length)].toUpperCase();
+console.log(word);
+
+
+window.onload = function() {
+    initialize();
+}
+function initialize(){
+    for (let r =0; r < height; r++){
+        for(let c = 0; c < width; c++){
+            let tile =document.createElement("span");
+            tile.id = r.toString()+"-"+ c.toString();
+            tile.classList.add("tile");
+            tile.innerText = " ";
+            document.getElementById("board").appendChild(tile);
+
+        }
+    }
+}
