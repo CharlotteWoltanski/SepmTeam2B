4673601ba3feef68e8b8d58fde11b63261fdfe73
Francis Kiel Bustamante <fkangelo@gmail.com>
2022-05-18 20:38:27 +1000

    PHP Implementation - First Run
    Migrated the original HTML code into fragmented PHP files.

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php $title = "WORDLE"; ?>
+<?php include('header.php'); ?>
+
+
+
+
+<body>
+    <h1 id="title"> Wordle </h1>
+
+    <div id="board"></div>
+    <div id="message container"></div>
+    <h1 id="answer"></h1>
+    </div>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('footer.php'); ?>
