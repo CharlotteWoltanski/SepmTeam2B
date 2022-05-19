44b7dcf26173b3c0e70669f79dbaf5f13e39da5e
chris-kr <32232896+chris-kr@users.noreply.github.com>
2022-05-19 22:54:01 +1000

    Added navbar and statistics modal

--- src/main/php/index.php
+++ src/main/php/index.php
@@ -1,5 +1,8 @@
 <?php $title = "WORDLE"; ?>
-<?php include('header.php'); ?>
+<?php
+    include('header.php');
+    include('navbar.php');
+?>
 
 
 
@@ -10,6 +13,27 @@
     <div id="board"></div>
     <div id="message container"></div>
     <h1 id="answer"></h1>
+
+    <div class="container">
+      <h2>Large Modal</h2>
+      <!-- Trigger the modal with a button -->
+
+      <!-- Modal -->
+      <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+              <p>This is a large modal.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </body>
 
